14e7e1d4bc004391adacd29d565d922c336d9f61
ReyCristian <Rey.cristian.eze@gmail.com>
2024-11-16 00:42:29 -0300

    Prueba de menu

---
+++ Assets/MenuController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+  public void OnButtonClicked()
+    {
+        Debug.Log("¡Botón presionado!");
+        // Aquí pones la lógica que quieres ejecutar
+    }
+}
