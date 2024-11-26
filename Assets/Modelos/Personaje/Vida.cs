5bd1b209b40bb3ec2ed413a6bd3c1834d331858e
ReyCristian <Rey.cristian.eze@gmail.com>
2024-11-26 02:03:26 -0300

    Sonidos

--- Assets/Modelos/Personaje/Vida.cs
+++ Assets/Modelos/Personaje/Vida.cs
@@ -14,11 +14,16 @@ public class Vida : MonoBehaviour
 
     public delegate void MuerteEvent(ListasTexturas.TexturasPersonaje skin);
     public event MuerteEvent Muerte;
+    public AudioSource audioSource; // Asigna el AudioSource desde el Inspector
+    public AudioClip deathSound;   // Asigna el sonido de muerte desde el Inspector
+
 
     private void Awake()
     {
         personaje = GetComponent<Personaje>();
         animator = GetComponentInChildren<Animator>();
+        audioSource = GetComponentInParent<AudioSource>();
+
     }
     // Start is called before the first frame update
     void Start()
@@ -56,8 +61,6 @@ public class Vida : MonoBehaviour
             return;
         }
         vida =0;
-        // Ejecuta la animación de muerte
-        //animacion.Morir();
 
         // Emite el evento si el personaje es un enemigo
         if (personaje && CompareTag("Enemigo"))
@@ -69,6 +72,10 @@ public class Vida : MonoBehaviour
         if (!CompareTag("Demo"))
         {
             muerto = true;
+            if (audioSource != null && deathSound != null)
+            {
+                audioSource.PlayOneShot(deathSound);
+            }
             if (animator !=null)
             {
                 animator.SetTrigger("Muere");
